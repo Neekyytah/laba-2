322c5b94ab8a66c76c2b1ea1159959d774a8fcf4
Neekyytah <thisissparta1337228@gmail.com>
2024-10-16 20:30:36 +0500

    Add files via upload

---
+++ examples/ex1.py
@@ -0,0 +1,10 @@
+print ("Пример простейшей функции без параметров:")
+def example():
+    print("Hello, World! (c) Smirnov")
+example()
+print ("C использованием return:")
+def example():
+    return True
+example = example()
+print(type(example))
+print(example)
---
+++ examples/ex4.py
@@ -0,0 +1,15 @@
+print("Пример функции, которая выводит список прочитанных книг:")
+def book_list(books, func):
+    for book in books:
+        print(func(book))
+books = ['System Design','Python и DevOps','Git. Практическое руководство']
+def book_print(book):
+    return book.upper() + ' - прочитано'
+book_list(books, book_print)
+
+print("Код, указанный выше, c использованием лямбда-функции:")
+def book_list(books, func):
+    for book in books:
+        print(func(book))
+books = ['System Design','Python и DevOps','Git. Практическое руководство']
+book_list(books, lambda book: book.upper() + ' - прочитано')
